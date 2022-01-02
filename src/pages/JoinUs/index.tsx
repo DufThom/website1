9eadae15fa438ad42f3c69377154f42821f303bb
Lucas <lucasyv12@gmail.com>
2022-01-02 17:59:36 +0100

    feat: add join us

---
+++ src/pages/JoinUs/index.tsx
@@ -0,0 +1,12 @@
+import { Layout } from '../../layout/Layout';
+import GoogleForm from '../../components/GoogleForm';
+
+const Home = () => {
+  return (
+    <Layout>
+      <GoogleForm />
+    </Layout>
+  );
+};
+
+export default Home;
