4000ba85b96eefbe34e6ff2e490457fa1562d3a4
a.dagnicourt <dagnicourt.a@gmail.com>
2022-01-10 16:35:41 +0100

    feat: update timeline frames

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,44 +1,50 @@
 export const projects = [
   {
     title: 'Star Atlas Métaverse',
-    description: "Star Atlas est un métaverse de jeu alimenté et sécurisé par la blockchain Solana (ultra-rapide). Les joueurs sont plongés au milieu d'un conflit intergalactique et se lancent dans une odyssée qui les mènera aux confins de la galaxie pour gagner du poids dans l'une des trois principales factions qui maintiennent la paix universelle. ",
-      image: '/images/mud_faction.webp',
-      tags: ['Star Atlas', 'Solana', 'Crypto', 'PlayToEarn'],
-    visit: 'https://staratlas.com/newsroom/article/star-atlas-a-day-in-the-life-of-a-mud-faction-captain',
+    description:
+      "Star Atlas est un métaverse de jeu alimenté et sécurisé par la blockchain Solana (ultra-rapide). Les joueurs sont plongés au milieu d'un conflit intergalactique et se lancent dans une odyssée qui les mènera aux confins de la galaxie pour gagner du poids dans l'une des trois principales factions qui maintiennent la paix universelle. ",
+    image: '/images/mud_faction.webp',
+    tags: ['Star Atlas', 'Solana', 'Crypto', 'PlayToEarn'],
+    visit:
+      'https://staratlas.com/newsroom/article/star-atlas-a-day-in-the-life-of-a-mud-faction-captain',
     id: 0,
   },
   {
     title: 'Commerce Galactic',
-    description:"Star Atlas combine la technologie blockchain de pointe avec un métavers de la taille d'une galaxie où les joueurs peuvent explorer et gagner leur odyssée à travers les profondeurs les plus impressionnantes de l'espace extra-atmosphérique.",
+    description:
+      "Star Atlas combine la technologie blockchain de pointe avec un métavers de la taille d'une galaxie où les joueurs peuvent explorer et gagner leur odyssée à travers les profondeurs les plus impressionnantes de l'espace extra-atmosphérique.",
     image: '/images/miningsite-01.webp',
     tags: ['investissement', 'vaisseaux', 'exploration'],
     // source: 'https://google.com',
-    visit: 'https://staratlas.com/newsroom/article/how-star-atlas-uses-the-blockchain-to-create-a-unique-play-to-earn-metaverse',
+    visit:
+      'https://staratlas.com/newsroom/article/how-star-atlas-uses-the-blockchain-to-create-a-unique-play-to-earn-metaverse',
     id: 1,
   },
   {
     title: 'Le mini-jeu',
-    description: "Le module SCORE — Ship Commission on Remote Expedition — consiste à approvisionner votre flotte en fournitures : des piles à hydrogène, de la nourriture, des munitions balistiques de base et des kits de réparation de coque à réparer après des combats armés et des éraflures causées par des débris spatiaux.",
-      image: '/images/pearceF4_fighter.webp',
-      tags: ['vaisseaux', 'galactics', 'flotte', 'SCORE'],
+    description:
+      'Le module SCORE — Ship Commission on Remote Expedition — consiste à approvisionner votre flotte en fournitures : des piles à hydrogène, de la nourriture, des munitions balistiques de base et des kits de réparation de coque à réparer après des combats armés et des éraflures causées par des débris spatiaux.',
+    image: '/images/pearceF4_fighter.webp',
+    tags: ['vaisseaux', 'galactics', 'flotte', 'SCORE'],
     // source: 'https://google.com',
     visit: 'https://medium.com/star-atlas/faction-fleet-service-gameplay-star-atlas-8e18421d01d9',
     id: 2,
   },
   {
     title: 'Star Atlas écosystème ',
-    description: "Star Atlas est un métaverse de jeu de nouvelle génération émergeant de la confluence d'une blockchain de pointe, de graphismes en temps réel, d'un jeu vidéo multijoueur et de technologies financières décentralisées.",
+    description:
+      "Star Atlas est un métaverse de jeu de nouvelle génération émergeant de la confluence d'une blockchain de pointe, de graphismes en temps réel, d'un jeu vidéo multijoueur et de technologies financières décentralisées.",
     image: '/images/commerce_star_Atlas.webp',
     tags: ['Star Atlas', 'Ecosystème', 'blockchain'],
-    visit: 'https://staratlas.com/newsroom/article/star-atlas-intergalactic-economy-101-welcome-to-the-real-world',
+    visit:
+      'https://staratlas.com/newsroom/article/star-atlas-intergalactic-economy-101-welcome-to-the-real-world',
     id: 3,
   },
 ];
 
 export const TimeLineData = [
-  { year: '12/2021', text: 'Naissance du métaverse Star Atlas', },
-  { year: '01/2022', text: 'Fusion des guildes francophones majeurs', },
-  { year: '02/2022', text: 'Structuration interne de la guilde', },
-  { year: '04/2022', text: 'Intialisation du DAO Eclypse', },
-  { year: '01/2023', text: 'Lancement du métaverse ', },
+  { year: '01/2022', text: 'Website V1' },
+  { year: ' 04/2022', text: 'Website V2' },
+  { year: '05/2022', text: 'DAO' },
+  { year: '12/2022', text: 'Gaming' },
 ];
