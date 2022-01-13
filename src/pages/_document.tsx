63805da677bc9b3044bf5efef96e6a29de7137c6
Catwonyl <catwonyl@gmail.com>
2022-01-13 01:12:29 +0100

    Test et changement de l'onglet de la fenêtre

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,11 +30,8 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
-          <link
-            href='https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap'
-            rel='stylesheet'
-          />
-          <link href='/fonts/style.css' rel='stylesheet' />
+          <title>Eclypse</title>
+          <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
